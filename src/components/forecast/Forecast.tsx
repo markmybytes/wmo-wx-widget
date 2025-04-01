5291b0d1b245f16f033b824b83e8cd4d3bff0ea1
markmybytes <markmybytes@gmail.com>
2025-04-01 20:33:00 +0800

    Remove usused code block

--- src/components/forecast/Forecast.tsx
+++ src/components/forecast/Forecast.tsx
@@ -88,49 +88,6 @@ export default async function Forecasts({
             </div>
           </div>
         );
-
-        return (
-          <div
-            className="flex flex-col flex-1 h-auto dark:text-gray-400"
-            key={idx}
-          >
-            <div
-              className="flex flex-col justify-center items-center"
-              style={{fontSize: "0.75rem"}}
-            >
-              <span
-                className="text-gray-400 dark:text-gray-500"
-                style={{
-                  whiteSpace: "nowrap",
-                  overflow: "ellipsis",
-                }}
-              >
-                {_d.toLocaleString(wmo.wmoToIso639(locale), {weekday: "long"})}{" "}
-              </span>
-              <span>{_d.getDate()}</span>
-            </div>
-
-            <div
-              className="flex justify-center items-center my-2"
-              style={{minWidth: "30%"}}
-            >
-              <img src={wx.icon!} width={50} height={30} />
-            </div>
-
-            <div className="flex flex-col lg:flex-row justify-center items-center">
-              <div className="flex justify-between mx-1 min-w-12 text-sky-600 dark:text-sky-700">
-                {flength >= 7 ? "" : <i className="bi bi-thermometer-low"></i>}
-
-                {`${wx.temp.min.val ?? "--"}${wx.temp.min.unit}`}
-              </div>
-              <div className="flex justify-between mx-1 min-w-12 text-red-600 dark:text-red-700">
-                {flength >= 7 ? "" : <i className="bi bi-thermometer-high"></i>}
-
-                {`${wx.temp.max.val ?? "--"}${wx.temp.max.unit}`}
-              </div>
-            </div>
-          </div>
-        );
       })}
     </div>
   );
